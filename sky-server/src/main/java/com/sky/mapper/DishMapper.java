247025af8f9bafb9ee9ac1917c95d842a8610629
programmer0721 <2642583636@qq.com>
2026-09-11 18:33:35 +0800

    新增菜品分页查询功能

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,13 +1,17 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface DishMapper {
+
 
     /**
      * 根据分类id查询菜品数量
@@ -20,4 +24,7 @@ public interface DishMapper {
     //新增菜品
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
+
+    //菜品分页查询
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,8 +1,12 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 //新增菜品及其口味
 public interface DishService {
     public void saveWithFlavor(DishDTO dishDTO);
+//菜品分页查询
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
